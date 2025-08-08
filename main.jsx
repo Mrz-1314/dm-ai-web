ad8a9de77533e5dad78dea4fd24c21080858422d
Mrz-1314 <zouxinhao007@gmail.com>
2025-08-08 16:34:31 +0900

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import DMAIWeb from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <DMAIWeb />
-  </React.StrictMode>,
-);
